84417859c82d7f1c77ac282cc5a1c645e4730bf3
abdullahh-dev <abdullahaha101@gmail.com>
2024-09-04 18:32:09 +0500

    removed some styles

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar = () => {
     <nav>
       <div
         style={{ WebkitBorderRadius: "12px" }}
-        className="bottom-[4%] z-[1000] flex fixed cst-md:hidden flex-col outline outline-1 outline-offset-0 outline-[#151515] p-1 right-[2%] overflow-hidden  backdrop-blur-[2px] rounded-[12px]"
+        className="bottom-[4%] z-[1000] flex fixed cst-md:hidden flex-col border border-[#151515] p-1 right-[2%] overflow-hidden  backdrop-blur-[2px] rounded-[12px]"
       >
         <div
           className={`transition-height duration-1000 flex flex-col items-center gap-5 overflow-hidden`}
